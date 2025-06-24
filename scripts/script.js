84631aab00e60cd5d0fc8338b362ce6fd3c6a6e7
gustavosrosa <g.dsantosrosa@gmail.com>
2025-06-24 11:03:27 -0300

    HRDG: FEAT: Finalizacao relogio

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
-console.log("Start")
+function useDate() {
+    let dateFromBrazilianHour = new Date();
+
+    document.getElementById("hours").textContent = addLeftZero(dateFromBrazilianHour.getHours().toString());
+    document.getElementById("minutes").textContent = addLeftZero(dateFromBrazilianHour.getMinutes().toString());
+    document.getElementById("seconds").textContent = addLeftZero(dateFromBrazilianHour.getSeconds().toString());
+
+    setTimeout('useDate()', 1000);
+}
+
+function addLeftZero(hour) {
+    if (hour.length == 1) return "0" + hour;
+    else return hour;
+}
